b3b2c627d1a66a57c413774e2278f7aff1acde6b
tlaxotjq <1212sts@naver.com>
2025-05-08 15:40:31 +0900

    fix: 버튼 클릭 이벤트 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2025webapp1'
+  base: '/2025webapp1',
+  build: {
+    outDir: 'dist', // 빌드 결과물이 'dist' 폴더에 저장되도록 설정
+  },
 })
